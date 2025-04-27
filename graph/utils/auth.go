e255225fb32779089e27d5f3bdd631d62bf799cd
Mykola Manko <mankokolya@gmail.com>
2025-04-27 20:43:06 +0300

    Add functions to extract token metadata from request and check token is valid

--- graph/utils/auth.go
+++ graph/utils/auth.go
@@ -1,11 +1,90 @@
 package utils
 
 import (
+	"net/http"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
 )
+
+type TokenMetadata struct {
+	Expires int64
+	UserId  string
+}
+
+func ExtractTokenMetadata(r *http.Request) (*TokenMetadata, error) {
+	token, err := verifyToken(r)
+
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+
+	var isValid bool = ok && token.Valid
+
+	if isValid {
+		expires := int64(claims["exp"].(float64))
+		userId := claims["userId"].(string)
+		return &TokenMetadata{
+			Expires: expires,
+			UserId:  userId,
+		}, nil
+	}
+
+	return nil, err
+}
+
+func verifyToken(r *http.Request) (*jwt.Token, error) {
+	var tokenString string = extractToken(r)
+
+	token, err := jwt.Parse(tokenString, jwtKeyFunc)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
+}
+
+func jwtKeyFunc(token *jwt.Token) (interface{}, error) {
+	return []byte(GetValue("JWT_SECRET_KEY")), nil
+}
+
+func extractToken(r *http.Request) string {
+	var header string = r.Header.Get("Authorization")
+
+	token := strings.Split(header, " ")
+
+	var isEmpty bool = header == "" || len(token) < 2
+
+	if isEmpty {
+		return ""
+	}
+
+	return token[1]
+}
+
+// CheckToken checks JWT token
+func CheckToken(r *http.Request) (*TokenMetadata, error) {
+    var now int64 = time.Now().Unix()
+
+    claims, err := ExtractTokenMetadata(r)
+    
+    if err != nil {
+        return nil, err
+    }
+
+    var expires int64 = claims.Expires
+
+    if now > expires {
+        return nil, err
+    }
+
+    return claims, nil
+}
 
 func GenerateNewAccessToke(userId string) (string, error) {
 	secret := GetValue("JWT_SECRET_KEY")
